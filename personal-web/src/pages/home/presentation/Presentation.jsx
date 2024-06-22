df77168d8292064ee68d12faa962828d80c7956c
VilattaRomina <vilattaromina@gmail.com>
2024-06-22 14:30:52 -0300

    cambios

--- personal-web/src/pages/home/presentation/Presentation.jsx
+++ personal-web/src/pages/home/presentation/Presentation.jsx
@@ -7,7 +7,7 @@ const Presentation = () => {
       className="md:grid md:grid-cols-3 gap-3 grid-cols-1 md:justify-between content-center items-center section max-w-4xl mx-auto p-5" 
       id="presentation"
     >
-      <div className="col-span-2 text-center md:text-left p-5">
+      <div className="md:col-span-2 col-span-1 text-start md:text-left p-5">
         <div className="text-2xl text-light font-normal flex flex-col gap-10">
         <h1 className="bg-clip-text-custom gradient-bg md:text-6xl text-5xl">
         Software Developer
@@ -16,7 +16,7 @@ const Presentation = () => {
           </p>
         </div>
       </div>
-      <div className="col-span-1 flex justify-center md:justify-end ">
+      <div className="md:col-span-1 col-span-1 text-start  md:justify-end ">
         <img
           src={perfil}
           alt="perfil"
